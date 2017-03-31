bf6c7f1d801b0bd2ac4dd6609fa1fceac186d735
Dušan Lago <dusan.lago@mail.muni.cz>
2017-03-31 16:49:54 +0200

    New model

--- application/router/api/index.js
+++ application/router/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const young = require('../../module/1phase');
+const youngNew = require('../../module/1phaseNew');
 const Face = require('../../model/Face');
 
 router.get('/faces', (req, res) => {   
@@ -7,8 +8,39 @@ router.get('/faces', (req, res) => {
 })
 
 router.post('/calc', (req, res, next) => {
-  const Ys = [];
-  for (i = 0; i< 1000; i++) Ys.push(young(req.body.matrix));
+  const Ys = {
+    x: [],
+    y: [],
+    z: [],
+    Y: []
+  };
+
+  for (i = 0; i< 100; i++) {
+    let {x, y, z, Y} = young(req.body.matrix);
+    Ys.x.push(x);
+    Ys.y.push(y);
+    Ys.z.push(z);
+    Ys.Y.push(Y);
+  }
+
+  return res.json(Ys);
+})
+
+router.post('/calcNew', (req, res, next) => {
+  const Ys = {
+    x: [],
+    y: [],
+    z: [],
+    Y: []
+  };
+
+  for (i = 0; i< 1000; i++) {
+    let {x, y, z, E} = youngNew(req.body.matrix);
+    Ys.x.push([x]);
+    Ys.y.push([y]);
+    Ys.z.push([z]);
+    Ys.Y.push(E);
+  }
 
   return res.json(Ys);
 })
