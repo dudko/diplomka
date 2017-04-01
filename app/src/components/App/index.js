fdf86987bf9b7d87d020409947dd2768f7863bdf
Dušan Lago <dusan.lago@mail.muni.cz>
2017-04-01 19:21:59 +0200

    Fast typing matrix form

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -12,40 +12,94 @@ export class App extends Component {
     super(props);
 
     this.state = {
-      material: {},
-      type: ''
-    }
-  
-    this.submitConsts = this.submitConsts.bind(this);
-    this.constEntered = this.constEntered.bind(this);
-    this.typeSelected = this.typeSelected.bind(this);
-  }
+      material: {
+        type: 'unknown',
+        constants: [0, 1, 2, 3, 4, 5].map(row =>
+          [0, 1, 2, 3, 4, 5].map(cell =>
+            ({ value: null, disabled: false })
+        ))
+       }
+    };
 
-  submitConsts(event) {
-    console.log(event)
+    this.constEntered = this.constEntered.bind(this);
+    this.materialTypeChanged = this.materialTypeChanged.bind(this);
+    this.diagonalSymmetry = this.diagonalSymmetry.bind(this);
   }
 
   constEntered(event) {
-    const cell = event.target.attributes.name.nodeValue;
-    const updatedMaterial = {
-      ...this.state.material,
-      [cell]: event.target.value
-    };
+    const [row, cell] = event.target.attributes.name.nodeValue.split('-');
+    const newMaterial = {...this.state.material};
 
-    if (this.state.type) {
-      updatedMaterial[`${cell[1]}${cell[0]}`] = updatedMaterial[`${cell[0]}${cell[1]}`]
+    newMaterial.constants[row][cell].value = event.target.value;
+    if (this.state.material.type !== 'unknown') {
+      newMaterial.constants = this.diagonalSymmetry(newMaterial.constants);
     }
     
     this.setState({
-      material: updatedMaterial
+      material: newMaterial
     });
   }
 
-  typeSelected(event) {
+  diagonalSymmetry(matrix) {
+    let row = 0;
+    while (row < 6) {
+      let cell = row + 1;
+      while (cell < 6) {
+        matrix[cell][row].value = matrix[row][cell].value;
+        cell += 1;
+      }
+      row += 1;
+    }
 
-    this.setState({
-      type: event.target.value
-    });
+    return matrix;
+  }
+
+  materialTypeChanged(event) {
+    const typeName = event.target.value;
+    let newMaterial = {...this.state.material};
+
+    if (typeName !== this.state.material.type) {
+      switch (typeName) {
+        case 'cubic':
+          newMaterial.type = 'cubic';
+
+          [[0, 3], [0, 4], [0, 5],
+           [1, 3], [1, 4], [1, 5],
+           [2, 3], [2, 4], [2, 5],
+           [3, 4], [3, 5],
+           [4, 5]
+          ].forEach(([row, cell]) => {
+            newMaterial.constants[row][cell].disabled = true;
+            newMaterial.constants[row][cell].value = '0';
+          });
+          
+          let row = 5; 
+          while(row) {
+            let cell = row-1;
+            while(cell >= 0) {
+              newMaterial.constants[row][cell].disabled = true;
+              cell -= 1;
+            }
+            row -= 1;
+          }
+
+          newMaterial.constants = this.diagonalSymmetry(newMaterial.constants);
+
+          break;
+        default:
+          newMaterial = {
+            type: 'unknown',
+            constants: [0, 1, 2, 3, 4, 5].map(row =>
+              [0, 1, 2, 3, 4, 5].map(cell =>
+                ({ value: newMaterial.constants[row][cell].value || '', disabled: false })
+            ))
+          }
+      }
+
+      this.setState({
+        material: newMaterial
+      });
+    }
   }
 
   render() {
@@ -53,31 +107,27 @@ export class App extends Component {
 
     return (
       <div className="page">
-        <h1>Elasticity Tool</h1>
-
         <select
-          onChange={this.typeSelected}
+          onChange={this.materialTypeChanged}
         >
-          <option defaultValue>Material type</option>
-          {['cubic', 'hexagonal', 'tetragonal', 'trigonal', 'orthorombic']
+          {['unknown', 'cubic']
             .map(type => <option key={type} value={type}>{type}</option>)}
         </select>
         
         <form>
-          {[0, 1, 2, 3, 4, 5].map(row => {
-            return (
-              <div key={row}>
-              {[0, 1, 2, 3, 4, 5].map(col => 
+          {material.constants.map((row, rowIndex) => (
+            <div key={rowIndex}>
+              {row.map((cell, cellIndex) => (
                 <input
-                  key={`${row}${col}`}
-                  name={`${row}${col}`}
-                  value={material[`${row}${col}`] || ''}
+                  key={`${rowIndex}-${cellIndex}`}
+                  name={`${rowIndex}-${cellIndex}`}
+                  value={cell.value || ''}
                   onChange={this.constEntered}
+                  disabled={cell.disabled}
                  />
-              )}
-              </div>
-            );
-          })}
+              ))}
+            </div>
+          ))}
           
           <button
             onClick={this.submitConsts}
