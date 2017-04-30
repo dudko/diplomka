213125804022aa254d1f52eb7f8751c76e29ab6b
Dušan Lago <dusan.lago@mail.muni.cz>
2017-04-30 09:50:37 +0200

    Composite UI  without redux

--- frontend/src/containers/CompositeContainer.js
+++ frontend/src/containers/CompositeContainer.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux';
-import { submitComposite, processPoints } from '../actions';
-
 import ElasticityInput from './ElasticityInput';
 import MaterialProjectSearch from '../components/MaterialProjectSearch';
-import Button from '../components/Button';
 import CompositeRatio from '../components/CompositeRatio';
 import CompositeRotation from '../components/CompositeRotation';
 import CrystalSystemSelect from './CrystalSystemSelect';
@@ -13,12 +9,30 @@ import CrystalSystemSelect from './CrystalSystemSelect';
 import RangeRun from './RangeRun';
 
 import Chart from '../components/Chart';
-import PropertiesContainer from './PropertiesContainer';
+import Properties from '../components/Properties';
 
-class CompositeContainer extends Component {
+import _ from 'lodash';
+const createWorker = require('worker-loader!../worker');
+
+export default class CompositeContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      elasticities: [0, 1].map(() => [0, 1, 2, 3, 4, 5].map(row =>
+        [0, 1, 2, 3, 4, 5].map(cell =>
+          ({ value: 0, disabled: false })
+      ))),
+      crystalSystems: ['any', 'any'],
+      results: {
+        x: [],
+        y: [],
+        z: [],
+        youngs: [],
+        compress: []
+      },
+      worker: new createWorker(),
+      elateAnalysis: [],
+      redraw: false,
       ratio: 0.5,
       rotation: {
         x: 0,
@@ -29,41 +43,43 @@ class CompositeContainer extends Component {
   }
 
   render() {
-    const { elasticities, points, worker, submitComposite } = this.props;
-    const { ratio, rotation } = this.state;
+    const { elasticities, crystalSystems, results, worker, elateAnalysis,
+      redraw, ratio, rotation } = this.state;
 
     return (
       <div>
         <div className='flex two'>
           <Chart
             key={'youngs'}
-            points={points}
-            propertyName={'Y'}
+            points={results}
+            redraw={redraw}
+            propertyName={'youngs'}
           />
           <Chart
-            key={'compressiblity'}
-            points={points}
-            propertyName={'compressiblity'}
+            key={'compress'}
+            points={results}
+            redraw={redraw}
+            propertyName={'compress'}
           />
         </div>
         <RangeRun />
-        
-        <hr />
 
-        <Button
+        <Properties
+          tables={elateAnalysis}
+        />
+
+        <button
+          type='button'
           className='success'
-          onClick={() =>
-            submitComposite({
-              elasticities: [
-                elasticities['2'].map(row => row.map(cell => cell.value)),
-                elasticities['3'].map(row => row.map(cell => cell.value))
-              ],
-              ratio,
-              rotation,
-          }, worker)}
+          onClick={() => {
+            const elasticityValues = elasticities.map(elasticity =>
+              elasticity.map(row => row.map(cell => cell.value)));
+            worker.postMessage(elasticityValues);
+            worker.onmessage = msg => this.setState({ results: msg.data, redraw: true });
+          }}
         >
           Submit
-        </Button>
+        </button>
 
         <hr />
 
@@ -77,49 +93,87 @@ class CompositeContainer extends Component {
           <CompositeRatio
             updateRatio={(value) => this.setState({ ratio: value})}
           />
-          
         </div>
 
-        <hr />
-        
         <div className='flex two'>
           <div>
             <h3>First material</h3>
-            <CrystalSystemSelect />
+
+            <CrystalSystemSelect
+              crystalSystem={crystalSystems[0]}
+              setSelectedCrystalSystem={(value) => this.setState({
+                crystalSystem: value
+              })}
+            />
+
             <ElasticityInput
-              id={'2'}
+              elasticity={elasticities[0]}
+              setConstant={(value, index) => {
+                const nextElasticities = _.cloneDeep(elasticities);
+                nextElasticities[0][index.row][index.cell].value = value; 
+                this.setState({
+                  elasticities: nextElasticities
+                });
+              }}
             />
           
-          <MaterialProjectSearch
-            elasticityId={'2'}
+            <MaterialProjectSearch
+              setElasticity={(foundElasticity, foundCrystalSystem) => {
+                const nextElasticities = _.cloneDeep(elasticities);
+                nextElasticities[0] = nextElasticities[0].map((row, rowIndex) =>
+                  row.map((cell, cellIndex) =>
+                    ({...cell, value: foundElasticity[rowIndex][cellIndex]})
+                ));
+                const nextCrystalSystems = _.cloneDeep(crystalSystems)
+                this.setState({
+                  elasticities: nextElasticities,
+                  crystalSystems: nextCrystalSystems
+                })
+              }}
           />
+
           </div>
 
           <div>
             <h3>Second material</h3>
-            <CrystalSystemSelect />
+
+            <CrystalSystemSelect
+              crystalSystem={crystalSystems[1]}
+              setSelectedCrystalSystem={(value) => this.setState({
+                crystalSystem: value
+              })}
+            />
+
             <ElasticityInput
-              id={'3'}
+              elasticity={elasticities[1]}
+              setConstant={(value, index) => {
+                const nextElasticities = _.cloneDeep(elasticities);
+                nextElasticities[1][index.row][index.cell].value = value; 
+                this.setState({
+                  elasticities: nextElasticities
+                });
+              }}
             />
+          
             <MaterialProjectSearch
-              elasticityId={'3'}
+              setElasticity={(foundElasticity, foundCrystalSystem) => {
+                const nextElasticities = _.cloneDeep(elasticities);
+                nextElasticities[1] = nextElasticities[1].map((row, rowIndex) =>
+                  row.map((cell, cellIndex) =>
+                    ({...cell, value: foundElasticity[rowIndex][cellIndex]})
+                ));
+                const nextCrystalSystems = _.cloneDeep(crystalSystems)
+                this.setState({
+                  elasticities: nextElasticities,
+                  crystalSystems: nextCrystalSystems
+                })
+              }}
             />
-          </div>
+
+          </div>          
+
         </div>
       </div>
     )
   }
-} 
-
-const mapStateToProps = (state) => ({
-  elasticities: state.elasticities,
-  points: state.points,
-  worker: state.worker,
-});
-
-const mapDispatchToProps = {
-  submitComposite,
-  processPoints
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(CompositeContainer);
