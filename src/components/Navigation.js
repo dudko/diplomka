0ac8b8e0a8f642dd72ef357e1daae42448801a04
Dušan Lago <dusan.lago@mail.muni.cz>
2017-12-03 18:13:53 +1000

    Improving flow

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default ({ location }) =>
+export default ({ location }) => (
   <div className="ui fixed stackable menu">
     <div className="ui container">
       <Link className="header item" to="/">
@@ -14,27 +14,47 @@ export default ({ location }) =>
         <i className="info icon" /> About
       </Link>
 
-      <div className="right secondary menu">
-        <Link
-          className={location.pathname === "/input" ? "active item" : "item"}
-          to="/input"
-        >
-          <i className="edit icon" /> Input
-        </Link>
-        <Link
-          className={location.pathname === "/adjust" ? "active item" : "item"}
-          to="/adjust"
-        >
-          <i className="settings icon" /> Adjust
-        </Link>
-        <Link
-          className={
-            location.pathname === "/calculate" ? "active item" : "item"
-          }
-          to="/calculate"
-        >
-          <i className="hourglass half icon" /> Calculate
-        </Link>
+      <div className="ui right">
+        <div className="ui three tiny steps">
+          <div
+            className={location.pathname === "/input" ? "active step" : "step"}
+          >
+            <i className="edit icon" />
+            <div className="content">
+              <Link to="/input">
+                <div className="title">Input</div>
+                <div className="description">elastic constants</div>
+              </Link>
+            </div>
+          </div>
+
+          <div
+            className={location.pathname === "/adjust" ? "active step" : "step"}
+          >
+            <i className="settings icon" />
+            <div className="content">
+              <Link to="/adjust">
+                <div className="title">Adjust</div>
+                <div className="description">rotations and fractions</div>
+              </Link>
+            </div>
+          </div>
+
+          <div
+            className={
+              location.pathname === "/calculate" ? "active step" : "step"
+            }
+          >
+            <i className="hourglass half icon" />
+            <div className="content">
+              <Link to="/calculate">
+                <div className="title">Compute</div>
+                <div className="description">elastic properties</div>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>;
+  </div>
+);
