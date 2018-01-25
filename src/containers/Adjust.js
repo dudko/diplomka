2ef49b4591039cfea42a2a377ff0e43907f67543
Dušan Lago <dusan.lago@mail.muni.cz>
2018-01-26 08:27:54 +1100

    Separating as components

--- src/containers/Adjust.js
+++ src/containers/Adjust.js
@@ -1,40 +1,41 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { push } from "react-router-redux";
-import { removeMatrix, resetMatrix, setFraction } from "../actions";
-import { rotateMatrix } from "../actions/workerActions";
-import { isValidFractionSum } from "../helpers";
-import Rotations from "../components/Rotations";
-import Fraction from "../components/Fraction";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { removeMatrix, resetMatrix, setFraction } from '../actions'
+import { rotateMatrix } from '../actions/workerActions'
+import { isValidFractionSum } from '../helpers'
+
+import Rotations from '../components/Rotations'
+import Fraction from '../components/Fraction'
+import NoMaterialsAdded from '../components/NoMaterialsAdded'
 
 class Adjust extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      invalidFraction: false
-    };
+      invalidFraction: false,
+    }
   }
 
   componentDidMount() {
-    const { materials } = this.props;
+    const { materials } = this.props
 
     if (!isValidFractionSum(materials)) {
       this.setState({
-        invalidFraction: true
-      });
+        invalidFraction: true,
+      })
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    const { materials, results } = nextProps;
+    const { materials, results } = nextProps
     this.state = {
-      results: results
-    };
+      results: results,
+    }
 
     if (!isValidFractionSum(materials)) {
       this.setState({
-        invalidFraction: true
-      });
+        invalidFraction: true,
+      })
     }
   }
   render() {
@@ -44,103 +45,91 @@ class Adjust extends Component {
       rotateMatrix,
       resetMatrix,
       setFraction,
-      push
-    } = this.props;
+    } = this.props
 
-    const { invalidFraction } = this.state;
+    const { invalidFraction } = this.state
+
+    if (!materials.size) return <NoMaterialsAdded />
+
     return (
-      <div>
-        {materials.size
-          ? <div className="ui centered grid">
-              <h1 className="ui header">
-                Adjust rotations (optional) and/or set fractions of phases in
-                the composite
-                <div className="sub header">
-                  Re-orientation of the [0, 0, 1] axis into a new direction and
-                  subsequent rotation around it
-                </div>
-              </h1>
-              {materials.map((material, key) =>
-                <div className="row">
-                  <div className="six wide column" key={key}>
-                    <table className="ui table">
-                      <thead>
-                        <th colSpan="6">
-                          <button
-                            className="ui mini red icon button"
-                            onClick={() => removeMatrix(key)}
-                          >
-                            <i className="close icon" />
-                          </button>
-                        </th>
-                      </thead>
-                      <tbody>
-                        {material.get("matrix").map((row, index) =>
-                          <tr key={index}>
-                            {row.map((cell, index) =>
-                              <td key={index}>
-                                {Number(cell) % 1
-                                  ? Number(cell).toFixed(3)
-                                  : Number(cell)}
-                              </td>
-                            )}
-                          </tr>
-                        )}
-                      </tbody>
-                    </table>
-                  </div>
-
-                  <div className="one wide column" />
-
-                  <div className="six wide column">
-                    <Rotations
-                      axes={material.get("axes")}
-                      angle={material.get("angle")}
-                      matrix={material.get("matrix")}
-                      rotateMatrix={(axes, angle) =>
-                        rotateMatrix(key, material.get("matrix"), axes, angle)}
-                      resetMatrix={() => resetMatrix(key)}
-                    />
-
-                    <br />
-                    <br />
-
-                    <Fraction
-                      fraction={material.get("fraction")}
-                      setFraction={fraction => setFraction(key, fraction)}
-                      invalidFraction={invalidFraction}
-                    />
-                  </div>
-                </div>
-              )}
+      <div className="ui centered grid">
+        <h1 className="ui left aligned header">
+          Optionaly adjust rotations and material ratios
+          <div className="sub header">
+            Re-orientation of the [0, 0, 1] axis into a new direction and
+            subsequent rotation around it
+          </div>
+        </h1>
+        {materials.map((material, index) => (
+          <div className="row" key={index}>
+            <div className="six wide column">
+              <table className="ui table">
+                <thead>
+                  <tr>
+                    <th colSpan="6">
+                      <button
+                        className="ui mini red icon button"
+                        onClick={() => removeMatrix(index)}
+                      >
+                        <i className="close icon" />
+                      </button>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {material
+                    .get('matrix')
+                    .map((row, index) => (
+                      <tr key={index}>
+                        {row.map((cell, index) => (
+                          <td key={index}>
+                            {Number(cell) % 1
+                              ? Number(cell).toFixed(3)
+                              : Number(cell)}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
             </div>
-          : <h3
-              style={{
-                textAlign: "center"
-              }}
-            >
-              Nothing to adjust. Add materials first.
+
+            <div className="one wide column" />
+
+            <div className="six wide column">
+              <Rotations
+                axes={material.get('axes')}
+                angle={material.get('angle')}
+                matrix={material.get('matrix')}
+                rotateMatrix={(axes, angle) =>
+                  rotateMatrix(index, material.get('matrix'), axes, angle)
+                }
+                resetMatrix={() => resetMatrix(index)}
+              />
+
               <br />
-              <div
-                className="ui green inverted button"
-                onClick={() => push("/input")}
-              >
-                <i className="edit icon" /> Input
-              </div>
-            </h3>}
+              <br />
+
+              <Fraction
+                fraction={material.get('fraction')}
+                setFraction={fraction => setFraction(index, fraction)}
+                invalidFraction={invalidFraction}
+              />
+            </div>
+          </div>
+        ))}
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => ({
-  materials: state.materials
-});
+  materials: state.materials,
+})
 
 export default connect(mapStateToProps, {
   removeMatrix,
   rotateMatrix,
   resetMatrix,
   setFraction,
-  push
-})(Adjust);
+})(Adjust)
--- src/containers/Calculate.js
+++ src/containers/Calculate.js
@@ -1,88 +1,72 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { push } from "react-router-redux";
-import Plot from "../components/Plot";
-import { Header, Modal } from "semantic-ui-react";
-import { isValidFractionSum } from "../helpers";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-import { calculate } from "../actions/workerActions";
+import { isValidFractionSum } from '../helpers'
+import { calculate } from '../actions/workerActions'
+
+import NoMaterialsAdded from '../components/NoMaterialsAdded'
+import Plot from '../components/Plot'
+import InvalidFractionModal from '../components/InvalidFractionModal'
 
 class Calculate extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       results: props.results,
       processing: false,
-      invalidFraction: false
-    };
+      invalidFraction: false,
+    }
   }
 
   componentDidMount() {
-    const { materials } = this.props;
+    const { materials } = this.props
 
     if (!isValidFractionSum(materials)) {
       this.setState({
-        invalidFraction: true
-      });
+        invalidFraction: true,
+      })
     }
   }
 
   componentWillReceiveProps(nextProps) {
-    const { materials, results } = nextProps;
+    const { materials, results } = nextProps
     this.state = {
       results: results,
-      processing: false
-    };
+      processing: false,
+    }
 
     if (!isValidFractionSum(materials)) {
       this.setState({
-        invalidFraction: true
-      });
+        invalidFraction: true,
+      })
     }
   }
 
   render() {
-    const { materials, calculate, push } = this.props;
-    const { processing, results, invalidFraction } = this.state;
+    const { materials, calculate } = this.props
+    const { processing, results, invalidFraction } = this.state
+
+    if (!materials.size) return <NoMaterialsAdded />
+    if (invalidFraction) return <InvalidFractionModal />
 
     return (
       <div className="ui centered grid">
-        <Modal open={invalidFraction} basic size="small">
-          <Header icon="warning circle" content="Fraction sum must be 1" />
-          <Modal.Content>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque
-              iure quos cupiditate, amet maxime similique hic quasi beatae
-              possimus aperiam illum enim quisquam numquam quibusdam neque!
-              Quidem reiciendis error porro!
-            </p>
-          </Modal.Content>
-          <Modal.Actions>
-            <div
-              className="ui green inverted button"
-              onClick={() => push("/adjust")}
-            >
-              <i className="settings icon" /> Adjust
-            </div>
-          </Modal.Actions>
-        </Modal>
-
         {results && (
           <div>
             <Plot
-              key={"youngs"}
+              key={'youngs'}
               points={results}
               redraw={true}
-              propertyName={"youngs"}
+              propertyName={'youngs'}
               title={"Young's modulus"}
-              unit={"GPa"}
+              unit={'GPa'}
             />
             <Plot
-              key={"compress"}
+              key={'compress'}
               points={results}
               redraw={true}
-              propertyName={"compress"}
-              title={"Linear compressibility"}
+              propertyName={'compress'}
+              title={'Linear compressibility'}
             />
           </div>
         )}
@@ -95,13 +79,13 @@ class Calculate extends Component {
               onClick={() => {
                 const materialsRaw = materials.reduce((result, material) => {
                   result.push({
-                    matrix: material.get("matrix"),
-                    fraction: material.get("fraction")
-                  });
-                  return result;
-                }, []);
-                calculate(materialsRaw);
-                this.setState({ processing: true });
+                    matrix: material.get('matrix'),
+                    fraction: material.get('fraction'),
+                  })
+                  return result
+                }, [])
+                calculate(materialsRaw)
+                this.setState({ processing: true })
               }}
             >
               Start calculations <i className="icon wizard" />
@@ -115,13 +99,13 @@ class Calculate extends Component {
           )}
         </div>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => ({
   materials: state.materials,
-  results: state.results
-});
+  results: state.results,
+})
 
-export default connect(mapStateToProps, { calculate, push })(Calculate);
+export default connect(mapStateToProps, { calculate })(Calculate)
