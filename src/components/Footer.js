f7dad5c271f3ce55017c086e5e1d0af15770ceb3
Dušan Lago <dusan.lago@mail.muni.cz>
2018-01-31 10:01:23 +1100

    Changing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logoIPM from '../assets/logoIPM.png'
+import logoCerit from '../assets/cerit-sc-logo.png'
+
 import Newsletter from './Newsletter'
 
 export default ({ count }) => (
@@ -8,28 +10,36 @@ export default ({ count }) => (
     <div className="ui container">
       <div className="ui stackable inverted divided equal height stackable grid">
         <div className="four wide column">
-          <h4 className="ui inverted header">Sitemap</h4>
-          <div className="ui inverted link list">
-            <Link className="item" to="/about">
-              About
-            </Link>
-          </div>
-          <Newsletter />
-        </div>
-        <div className="five wide column">
+          <i>Developed in cooperation with:</i>
           <img
             src={logoIPM}
             alt="Institute of Physics of Materials"
-            className="ui tiny image"
+            className="ui tiny footer image"
           />
-          <b>Institute of Physics of Materials</b>
           <p>
+            <b>Institute of Physics of Materials</b>
+            <br />
             Academy of Sciences of the Czech Republic<br />
-            Brno, Czech Republic<br />
           </p>
         </div>
+
+        <div className="four wide column">
+          <i>Supported by:</i>
+          <img
+            src={logoCerit}
+            alt="CERIT Scientific Cloud"
+            className="ui tiny footer image"
+          />
+          <p>
+            <b>CERIT Scientific Cloud</b>
+            <br />
+            Institute of Computer Sciences<br />
+            Masaryk University
+          </p>
+        </div>
+
         <div className="five wide column">
-          <h4 className="ui inverted header">MU</h4>
+          <Newsletter />
         </div>
       </div>
     </div>
