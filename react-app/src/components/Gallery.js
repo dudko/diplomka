a284ab8b4fce77fc70f03eb4371ac73c2c9b29f9
Dušan Lago <dusan.lago@gmail.com>
2018-12-12 20:38:18 +0100

    Gallery update

--- react-app/src/components/Gallery.js
+++ react-app/src/components/Gallery.js
@@ -1,18 +1,36 @@
 import React, { Component } from "react";
 import ImageGallery from "react-image-gallery";
+import Fe from "../assets/landing-page/Fe.png";
+import FeThumb from "../assets/landing-page/Fe-thumb.png";
+import Al from "../assets/landing-page/Al.png";
+import AlThumb from "../assets/landing-page/Al-thumb.png";
+import Fe3Al from "../assets/landing-page/Fe3Al.png";
+import Fe3AlThumb from "../assets/landing-page/Fe3Al-thumb.png";
 
 export default class Gallery extends Component {
   render() {
-    const images = [1, 2, 3, 4, 5, 6].map(i => ({
-      original: `./images/${i}.png`,
-      thumbnail: `./images/${i}-thumb.png`,
-      description: "Young's modulus of FeO",
-    }));
+    const materials = [
+      {
+        original: Fe,
+        thumbnail: FeThumb,
+        description: "Young's modulus of Fe compound.",
+      },
+      {
+        original: Al,
+        thumbnail: AlThumb,
+        description: "Young's modulus of Al compound.",
+      },
+      {
+        original: Fe3Al,
+        thumbnail: Fe3AlThumb,
+        description: "Young's modulus of Fe3Al composite.",
+      },
+    ];
 
     return (
       <ImageGallery
-        items={images}
-        slideInterval={3500}
+        items={materials}
+        slideInterval={5000}
         autoPlay={true}
         lazyLoad={true}
         showFullscreenButton={false}
